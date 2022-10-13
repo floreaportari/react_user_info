bf3d2d386c53bf2026d354f5267bd817094e24ac
Florian Portari <floryanportar@gmail.com>
2022-10-13 09:38:12 +0200

    minor changes

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,21 +1,42 @@
 import Button from "./Button";
 import styles from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm} />;
+};
+
+const OverlayModal = (props) => {
+  return (
+    <div className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onConfirm}>Close</Button>
+      </footer>
+    </div>
+  );
+};
 
 const ErrorModal = (props) => {
   return (
     <>
-      <div className={styles.backdrop} onClick={props.onConfirm} />
-      <div className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onConfirm}>Close</Button>
-        </footer>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <OverlayModal
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-modal")
+      )}
     </>
   );
 };
--- src/components/Users/UserInput.js
+++ src/components/Users/UserInput.js
@@ -1,31 +1,36 @@
 import styles from "./UserInput.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
 
 const UserInput = (props) => {
-  const [username, setUsername] = useState("");
-  const [age, setAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  // const [username, setUsername] = useState("");
+  // const [age, setAge] = useState("");
   const [error, setError] = useState();
 
-  const userNameHandler = (e) => {
-    setUsername(e.target.value);
-  };
-  const ageHandler = (e) => {
-    setAge(e.target.value);
-  };
+  // const userNameHandler = (e) => {
+  //   setUsername(e.target.value);
+  // };
+  // const ageHandler = (e) => {
+  //   setAge(e.target.value);
+  // };
 
   const submitHandler = (e) => {
     e.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
 
-    if (username.trim().length === 0 || age.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (no empty values)",
       });
       return;
     }
-    if (+age < 1) {
+    if (+enteredAge < 1) {
       setError({
         title: "Invalid age",
         message: "Please enter a valid age (> 0)",
@@ -33,9 +38,12 @@ const UserInput = (props) => {
       return;
     }
 
-    props.onAddUser(username, age);
-    setUsername("");
-    setAge("");
+    props.onAddUser(enteredName, enteredAge);
+    // setUsername("");
+    // setAge("");
+
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   const errorHandler = () => {
@@ -54,11 +62,21 @@ const UserInput = (props) => {
       <form className={styles.formControl} onSubmit={submitHandler}>
         <div className={styles.inputControl}>
           <label>Username</label>
-          <input type="text" value={username} onChange={userNameHandler} />
+          <input
+            type="text"
+            // value={username}
+            // onChange={userNameHandler}
+            ref={nameInputRef}
+          />
         </div>
         <div className={styles.inputControl}>
           <label>Age (Years)</label>
-          <input type="number" value={age} onChange={ageHandler} />
+          <input
+            type="number"
+            // value={age}
+            // onChange={ageHandler}
+            ref={ageInputRef}
+          />
         </div>
         <Button type="submit">Add User</Button>
       </form>
